24becc2bed22fc94de0e3c7f7db985776be29632
yunik <dmitry.yunitski@mapbox.com>
2022-03-17 00:28:59 +0200

    Update url.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function login() {
-  const url = "https://findmybus.auth.eu-west-1.amazoncognito.com/login?client_id=3vvpamg4sflamvg9lnfvav8btm&response_type=token&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Fexample.com%2Fcallback";
+  const url = "https://findmybus.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=3vvpamg4sflamvg9lnfvav8btm&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+profile&redirect_uri=https%3A%2F%2Fyunikkk.github.io%2Fvolunteer-buses";
 
   window.location.href = url;
 }
